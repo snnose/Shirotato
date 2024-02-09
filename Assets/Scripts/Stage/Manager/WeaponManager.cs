b3cc8081d5ebfa67403fcafd55a58dd0ac68b336
Seoko <teary1@naver.com>
2024-02-09 22:03:28 +0900

    Update WeaponManager.cs

--- Assets/Scripts/Stage/Manager/WeaponManager.cs
+++ Assets/Scripts/Stage/Manager/WeaponManager.cs
@@ -25,6 +25,9 @@ public class WeaponManager : MonoBehaviour
     public List<GameObject> currentWeaponList = new();
     private List<Vector2> weaponPos = new() { new Vector2(-2.0f, 0f), new Vector2(2.0f, 0f), };
 
+    private IEnumerator equipWeapons;
+    private IEnumerator destroyWeapons;
+
     private void Awake()
     {
         if (instance == null)
@@ -39,15 +42,41 @@ public class WeaponManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentWeaponList.Add(weapon);
         GameObject copy = Instantiate(weapon, weaponPos[0], Quaternion.Euler(0f, -180f, 0f)) as GameObject;
         copy.transform.SetParent(playerBox.transform, false);
-        currentWeaponList.Add(copy);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 라운드 시작 시 무기 장착하고 끝날 때 무기 파괴?
+        if (equipWeapons != null)
+        {
+            StartCoroutine(EquipWeapons());
+        }
+
+        if (destroyWeapons != null)
+        {
+            StartCoroutine(DestroyWeapons());
+        }
+    }
+
+    // 무기 장착 함수
+    public IEnumerator EquipWeapons()
+    {
+        yield return null;
+    }
+
+    // 무기 파괴 함수
+    public IEnumerator DestroyWeapons()
+    {
+        yield return null;
+    }
+
+    public IEnumerator GetEquipWeapons()
+    {
+        return this.equipWeapons;
     }
 
     public List<GameObject> GetCurrentWeaponList()

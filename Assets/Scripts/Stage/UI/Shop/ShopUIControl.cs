3166e34f266a346d092dafe0f0380fb540a0dd79
Seoko <teary1@naver.com>
2024-02-13 23:09:17 +0900

    Update ShopUIControl.cs

--- Assets/Scripts/Stage/UI/Shop/ShopUIControl.cs
+++ Assets/Scripts/Stage/UI/Shop/ShopUIControl.cs
@@ -6,16 +6,46 @@ using TMPro;
 
 public class ShopUIControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static ShopUIControl instance = null;
+
+    public static ShopUIControl Instance
+    {
+        get
+        {
+            if (null == instance)
+                return null;
+
+            return instance;
+        }
+    }
+
+    private ShopRerollButton shopRerollButton;
+    private ShopItemListControl shopItemListControl;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+
+        shopRerollButton = this.gameObject.transform.GetChild(8).gameObject.GetComponent<ShopRerollButton>();
+        shopItemListControl = this.gameObject.transform.GetChild(3).gameObject.GetComponent<ShopItemListControl>();
+    }
+
     void Start()
     {
         this.transform.position = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
         this.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public ShopRerollButton GetShopRerollButton()
     {
-        
+        return this.shopRerollButton;
+    }
+
+    public ShopItemListControl GetShopItemListControl()
+    {
+        return this.shopItemListControl;
     }
 }

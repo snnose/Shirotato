78a9d62d8e9459095404dd61bbeff8485965b839
Seoko <teary1@naver.com>
2024-03-07 13:38:57 +0900

    Create MilkControl.cs
    플레이어가 우유에 충돌 시 체력 회복

---
+++ Assets/Scripts/Stage/Goods/MilkControl.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MilkControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 라운드 종료 시 사라진다
+        if (GameRoot.Instance.GetIsRoundClear())
+            Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 플레이어와 맞닿을 경우
+        if (collision.gameObject == PlayerControl.Instance.GetPlayer())
+        {
+            // 플레이어의 체력을 회복한다
+            float currentHP = GameRoot.Instance.GetCurrentHP();
+            float maxHP = GameRoot.Instance.GetMaxHP();
+
+            currentHP += 3.0f;
+            // 최대 체력을 초과하지 않는다
+            if (currentHP >= maxHP)
+                currentHP = maxHP;
+
+            GameRoot.Instance.SetCurrentHP(currentHP);
+
+            // 텍스트를 출력한다
+            PrintText(collision.transform, 3);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void PrintText(Transform transform, int num)
+    {
+        // 대미지 텍스트 출력
+        GameObject text = Resources.Load<GameObject>("Prefabs/DamageText");
+        TextMeshPro textPro = text.GetComponent<TextMeshPro>();
+
+        // 텍스트 및 색상 결정
+        textPro.text = "+" + num.ToString();
+        textPro.color = Color.green;
+
+        GameObject copy = Instantiate(text);
+        Vector3 randomPos = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(0.4f, 0.6f), 0f);
+        copy.transform.position = transform.position + randomPos;
+    }
+}

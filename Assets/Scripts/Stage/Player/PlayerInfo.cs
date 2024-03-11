65b3b44fd827c0c50f2b8e7991e0053575827296
Seoko <teary1@naver.com>
2024-03-11 12:15:16 +0900

    Fixed movement speed
    아이템 발견 후 획득 시 이동속도가 비정상적으로 적용되는 현상 수정

--- Assets/Scripts/Stage/Player/PlayerInfo.cs
+++ Assets/Scripts/Stage/Player/PlayerInfo.cs
@@ -32,7 +32,7 @@ public class PlayerInfo : MonoBehaviour
     private int Evasion = 0;
 
     // 유틸 관련
-    private float MovementSpeed = 10f;
+    public float MovementSpeed = 10f;
     private float MovementSpeedPercent = 0f;
     private float RootingRange = 1f;
     private float Luck = 0f;

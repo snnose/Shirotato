fd07c68f85735cd991942ad1f9aab33574c43748
Seoko <teary1@naver.com>
2024-03-15 13:44:18 +0900

    Add Critical, HPDrain func
    공격 시 치명타와 생명력 흡수 기능 추가

--- Assets/Scripts/Stage/Weapon/BulletControl.cs
+++ Assets/Scripts/Stage/Weapon/BulletControl.cs
@@ -7,6 +7,7 @@ public class BulletControl : MonoBehaviour
 {
     private int damage = 0;
     private int pierceCount = 0;
+    private bool isCritical = false;
 
     // Start is called before the first frame update
     void Start()
@@ -36,11 +37,33 @@ public class BulletControl : MonoBehaviour
             GameObject hitedMonster = collision.gameObject;
             MonsterControl monsterControl = hitedMonster.GetComponent<MonsterControl>();
 
-            float hp = monsterControl.GetMonsterCurrentHP() - damage;
+            // 치명타 적용
+            float random = Random.Range(0f, 100f);
+            if (random < RealtimeInfoManager.Instance.GetCritical())
+                isCritical = true;
+
+            // 생명력 흡수 적용
+            random = Random.Range(0f, 100f);
+            if (random < RealtimeInfoManager.Instance.GetHPDrain()
+                && RealtimeInfoManager.Instance.GetCurrentHP() < RealtimeInfoManager.Instance.GetHP())
+            {
+                float currentHP = RealtimeInfoManager.Instance.GetCurrentHP();
+                currentHP += 1f;
+                RealtimeInfoManager.Instance.SetCurrentHP(currentHP);
+                PrintText(PlayerControl.Instance.transform, 1);
+            }
+
+            float finalDamage = damage;
+            if (isCritical)
+            {
+                finalDamage *= 1.5f;
+            }
+
+            float hp = monsterControl.GetMonsterCurrentHP() - finalDamage;
             monsterControl.SetMonsterCurrentHP(hp);
 
             // 입힌 대미지 출력
-            PrintText(hitedMonster.transform, damage);
+            PrintText(hitedMonster.transform, finalDamage, isCritical);
 
             // 관통 횟수가 1 이상이라면
             if (pierceCount > 0)
@@ -57,19 +80,56 @@ public class BulletControl : MonoBehaviour
         }
     }
 
-    void PrintText(Transform transform, int damage)
+    void PrintText(Transform transform, float damage, bool isCritical)
     {
         // 대미지 텍스트 출력
-        GameObject damageText = Resources.Load<GameObject>("Prefabs/DamageText");
-        TextMeshPro damagePro = damageText.GetComponent<TextMeshPro>();
+        GameObject textObject = Resources.Load<GameObject>("Prefabs/DamageText");
+        TextMeshPro tmPro = textObject.GetComponent<TextMeshPro>();
 
         // 텍스트 및 색상 결정
-        damagePro.text = damage.ToString();
-        damagePro.color = Color.white;
+        tmPro.text = damage.ToString();
+        Color color = Color.white;
+        if (isCritical)
+        {
+            color = Color.yellow;
+        }
+        tmPro.color = color;
 
-        GameObject copy = Instantiate(damageText);
+        GameObject copy = Instantiate(textObject);
         Vector3 randomPos = new Vector3(Random.Range(-0.4f, 0.4f), Random.Range(0.4f, 0.6f), 0f);
         copy.transform.position = transform.position + randomPos;
+    }
+
+    void PrintText(Transform transform, int num)
+    {
+        // 대미지 텍스트 출력
+        GameObject textObject = Resources.Load<GameObject>("Prefabs/DamageText");
+        TextMeshPro tmPro = textObject.GetComponent<TextMeshPro>();
+
+        // 텍스트 및 색상 결정
+        tmPro.text = num.ToString();
+        Color color = Color.white;
+        ColorUtility.TryParseHtmlString("#1FDE38", out color);
+        tmPro.color = color;
+
+        GameObject copy = Instantiate(textObject);
+        Vector3 randomPos = new Vector3(Random.Range(-0.4f, 0.4f), Random.Range(0.4f, 0.6f), 0f);
+        copy.transform.position = transform.position + randomPos;
+    }
+
+    void TryHPDrain()
+    {
+        float random = Random.Range(0f, 100f);
+
+        if (random < RealtimeInfoManager.Instance.GetCritical())
+        {
+            float currentHP = RealtimeInfoManager.Instance.GetCurrentHP();
+            if (currentHP < RealtimeInfoManager.Instance.GetHP())
+            {
+                currentHP += 1f;
+
+            }
+        }
     }
 
     public void SetDamage(int damage)

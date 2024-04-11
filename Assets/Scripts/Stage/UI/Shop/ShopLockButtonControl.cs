c4668820d63d54f40190cfe9ad30202c05c6ca16
Seoko <teary1@naver.com>
2024-04-11 16:54:57 +0900

    Add lock image
    상점 잠근 버튼 클릭 시 자물쇠 이미지가 활성화, 비활성화 됨

--- Assets/Scripts/Stage/UI/Shop/ShopLockButtonControl.cs
+++ Assets/Scripts/Stage/UI/Shop/ShopLockButtonControl.cs
@@ -11,42 +11,48 @@ public class ShopLockButtonControl : MonoBehaviour, IPointerEnterHandler, IPoint
     private Image background;
     private TextMeshProUGUI text;
 
+    private GameObject lockImage;
+
     private void Awake()
     {
         background = this.transform.GetChild(0).GetComponent<Image>();
         text = this.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        lockImage = this.transform.parent.GetChild(6).gameObject;
+
         lockButton = this.GetComponent<Button>();
         lockButton.onClick.AddListener(OnClickLockButton);
+
+        lockImage.SetActive(false);
     }
 
     public void OnClickLockButton()
     {
-        // 버튼의 X 좌표를 기반으로 몇번째 아이템 품목인지 찾는다.
-        float posX = this.transform.position.x - 0.5f;
-        int lockNum = FindLockItemNum(posX);
+        // 부모의 이름을 바탕으로 몇번째 아이템인지 찾는다.
+        string parentName = this.transform.parent.name;
+        int lockNum = FindLockItemNum(parentName);
 
         // 해당 아이템을 잠그거나 잠금 해제한다.
         LockItem(lockNum);
     }
 
-    // 잠근 버튼의 x좌표에 따라 몇번째 아이템에 상호 작용하는지 반환한다.
-    private int FindLockItemNum(float posX)
+    // 잠근 버튼의 부모 이름에 따라 몇번째 아이템에 상호 작용하는지 반환한다.
+    private int FindLockItemNum(string parentName)
     {
         int num = -1;
 
-        switch(posX)
+        switch(parentName)
         {
             // 첫번째 (0)
-            case 135:
+            case "ItemZero":
                 num = 0;
                 break;
-            case 380:
+            case "ItemOne":
                 num = 1;
                 break;
-            case 625:
+            case "ItemTwo":
                 num = 2;
                 break;
-            case 870:
+            case "ItemThree":
                 num = 3;
                 break;
             default:
@@ -67,14 +73,16 @@ public class ShopLockButtonControl : MonoBehaviour, IPointerEnterHandler, IPoint
         {
             // 잠금 해제한다.
             tmp[num] = false;
-            // 해당 아이템 칸을 잠긴 것 처럼 보이게 자물쇠 이미지 활성화
+            // 자물쇠 이미지 비활성화
+            lockImage.SetActive(false);
         }
         // 잠기지 않았다면
         else
         {
             // 잠근다.
             tmp[num] = true;
-            // 자물쇠 이미지 비활성화
+            // 해당 아이템 칸을 잠긴 것 처럼 보이게 자물쇠 이미지 활성화
+            lockImage.SetActive(true);
         }
 
         ItemManager.Instance.SetIsLockItemList(tmp);
